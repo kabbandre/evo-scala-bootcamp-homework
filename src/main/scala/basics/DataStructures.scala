8957f409a8b221716ddb1d771199f2d8cfa1e36a
Mihails Jeremejevs <Kabbandre@gmail.com>
2020-10-07 19:54:12 +0300

    HW4 Added

---
+++ src/main/scala/basics/DataStructures.scala
@@ -0,0 +1,40 @@
+package basics
+
+object DataStructures {
+  // Homework
+  //
+  // Implement a special sort which sorts the keys of a map (K) according to their associated
+  // values (V).
+  //
+  // In case of "ties" (equal values) it should group these keys K into Set-s in the results.
+  //
+  // The input is a map where keys (K) are the values to be sorted and values are their associated numeric
+  // values.
+  //
+  // The output is a list (in ascending order according to the associated `Int` values) of tuples of `Set`-s
+  // with values from K, and the associated value V for these values in the `Set`.
+  //
+  // For example:
+  //
+  // Input `Map("a" -> 1, "b" -> 2, "c" -> 4, "d" -> 1, "e" -> 0, "f" -> 2, "g" -> 2)` should result in
+  // output `List(Set("e") -> 0, Set("a", "d") -> 1, Set("b", "f", "g") -> 2, Set("c") -> 4)`.
+  def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = {
+    map
+      .groupBy(_._2)                  // Group by value
+      .transform((_, v) => v.keySet)  // Remove value from value set
+      .map(_.swap)                    // Swap key and value
+      .toList                         // Convert to list for sorting
+      .sortBy(_._2)                   // Sort by value
+  }
+  val input = Map(
+    "a" -> 1,
+    "b" -> 2,
+    "c" -> 4,
+    "d" -> 1,
+    "e" -> 0,
+    "f" -> 2,
+    "g" -> 2
+  )
+  val x = sortConsideringEqualValues(input)
+  x.map { case (k, _) => k.toList.sorted.mkString("=") }.mkString(" ")
+}
